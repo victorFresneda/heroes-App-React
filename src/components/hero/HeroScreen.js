10150fe82b743b74bce69e265978bcfd57b3cbef
victorFresneda <vafresnedah@hotmail.com>
2022-02-08 21:44:28 -0500

    Se ajuta a nivel visual el componente HeroScreen y se agrega boton de regreso al menu principal

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,13 +1,68 @@
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { getHerosById } from "../../selectors/getHerosById";
 
 
 export const HeroScreen = () => {
 
    const { heroeId } = useParams();
+
+   const hero = getHerosById( heroeId );
+
+   const navigate = useNavigate();
+
+   const handleReturn = () => {
+
+        
+      
+
+            navigate( -1, {
+                  replace: true
+              });
+
+      
+          
+      
+
+   }
+
+   if (!hero){
+        return < Navigate to='/' />
+   }
+
+   const imagePath = `/assets/${hero.id}.jpg`;
+
+
   return(
 
-        <div> 
-            <h1>Hero Screen</h1>
+        <div className="row mt-5">
+              <div className="col-4">
+                  <img src={ imagePath }
+                  alt={ hero.superhero}
+                  className="img-thumbnail" 
+                  
+                  />
+              </div>
+              <div className="col-8">
+                    <h3>{ hero.superhero }</h3>
+                    <ul className="list-group">
+                          <li className="list-group-item"> <b>Alter ego</b> {hero.alter_ego} </li>
+                          <li className="list-group-item"> <b>Publisher</b> {hero.publisher} </li>
+                          <li className="list-group-item"> <b>First appearence</b> {hero.first_appearance} </li>
+
+                    </ul>
+
+                    <h5>Characters</h5>
+                    <p>{ hero.characters}</p>
+
+                    <button
+                    className="btn btn-outline-info"
+                    onClick={ handleReturn }
+                    >
+                        Regresar      
+                    </button>
+
+              </div>
+
         </div>
   )
 };
---
+++ src/selectors/getHerosById.js
@@ -0,0 +1,10 @@
+import { heroes } from "../data/heroes"
+
+
+
+
+export const getHerosById = ( id = '') => {
+
+    return heroes.find( hero => hero.id === id );
+
+};
